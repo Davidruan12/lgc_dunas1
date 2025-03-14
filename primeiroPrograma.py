7d5b71847bbbdf6fcb1f6acabf8a564404fef483
DAVID RUAN PEREIRA DE SOUZA <202404116051@alunos.unifanor.edu.br>
2025-03-14 10:41:34 -0300

    primro commit

---
+++ primeiroPrograma.py
@@ -0,0 +1,7 @@
+nome = str(input("Qual é o seu nome? "))
+senha = str(input("Qual é a sua senha? "))
+
+if nome != "David" or senha != "123":
+    print("Nome ou senha incorretos!")
+else:
+    print("Bem vindo!")
